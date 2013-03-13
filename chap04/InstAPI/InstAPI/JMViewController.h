c27e31a4dbe22d9e06dbc43fc30c662fd9637796
Jorge Maroto <patoroco@gmail.com>
2013-03-13 13:26:16 +0100

    InstAPI

---
+++ chap04/InstAPI/InstAPI/JMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMViewController.h
+//  InstAPI
+//
+//  Created by Jorge Maroto Garcia on 13/3/13.
+//  Copyright (c) 2013 Tactilapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMViewController : UIViewController
+
+@end
---
+++ chap04/InstAPI/InstAPI/JMViewController.m
@@ -0,0 +1,29 @@
+//
+//  JMViewController.m
+//  InstAPI
+//
+//  Created by Jorge Maroto Garcia on 13/3/13.
+//  Copyright (c) 2013 Tactilapp.com. All rights reserved.
+//
+
+#import "JMViewController.h"
+
+@interface JMViewController ()
+
+@end
+
+@implementation JMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
