c76f0133ffe67f8f8f184de67ca1f360597f7ee5
Jorge Maroto <patoroco@gmail.com>
2013-04-22 19:19:27 +0200

    Adding ASI -> AFN chapter

---
+++ chap06/ASIHTTPtoAFN/ASIHTTPtoAFN/JMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMViewController.h
+//  ASIHTTPtoAFN
+//
+//  Created by Jorge Maroto Garcia on 22/4/13.
+//  Copyright (c) 2013 Tactilapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMViewController : UIViewController
+
+@end
---
+++ chap06/ASIHTTPtoAFN/ASIHTTPtoAFN/JMViewController.m
@@ -0,0 +1,29 @@
+//
+//  JMViewController.m
+//  ASIHTTPtoAFN
+//
+//  Created by Jorge Maroto Garcia on 22/4/13.
+//  Copyright (c) 2013 Tactilapp.com. All rights reserved.
+//
+
+#import "JMViewController.h"
+
+@interface JMViewController ()
+
+@end
+
+@implementation JMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
